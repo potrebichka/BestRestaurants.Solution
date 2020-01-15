e56a43b46c86cadd49574edf7e6d9fb6daf69c68
Nina Potrebich <potrebich@gmail.com>
2020-01-15 15:33:14 -0800

    Add Index route and page for Restaurant

---
+++ BestRestaurant/Controllers/RestarauntsControllers.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BestRestaurants.Controllers
+{
+  public class RestaurantsController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public RestaurantsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.Include(cuisines => cuisines.Cuisine).ToList();
+      return View(model);
+    }
+  }
+}
